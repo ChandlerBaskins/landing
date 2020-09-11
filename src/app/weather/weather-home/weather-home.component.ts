e40373179a86b4adf3ea06b274a495b4e1bef54e
Chandler Baskins <baskinschandler@gmail.com>
2020-09-11 13:23:39 -0500

    updates

--- src/app/weather/weather-home/weather-home.component.ts
+++ src/app/weather/weather-home/weather-home.component.ts
@@ -1,22 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { WeatherService } from '../weather.service';
+import { tap } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'weather',
   templateUrl: './weather-home.component.html',
 })
-export class WeatherHomeComponent implements OnInit {
-  userLocation$ = new Observable<Coordinates>((subscriber) => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        subscriber.next(position.coords);
-        subscriber.complete();
-      },
-      (err) => subscriber.error(err)
-    );
-  });
-
-  constructor() {}
+export class WeatherHomeComponent implements OnInit,OnDestroy {
+  userLocation$: Coordinates;
+  location
+  subscription: Subscription
+  constructor(private weatherService: WeatherService) {
+    //this.subscription = this.weatherService.userLocation$.subscribe(res => this.userLocation$ = res)
+    this.location = this.weatherService.forecast
+  }
 
   ngOnInit(): void {}
+  ngOnDestroy(){
+    //this.subscription.unsubscribe()
+  }
 }
--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
-
-  constructor() { }
+  forecast = this.getCurrentLocation().pipe(map((val) => val.accuracy));
+  getCurrentLocation() {
+    return new Observable<Coordinates>((subscriber) => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          subscriber.next(position.coords);
+          subscriber.complete();
+        },
+        (err) => subscriber.error(err)
+      );
+    }).pipe(tap((res) => console.log(res)));
+  }
+  constructor() {}
 }
