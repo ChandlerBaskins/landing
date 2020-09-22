36f464045d05c054b20ae0354637b02b13124e06
LAPTOP-TAR46155\Chandler Baskins <baskinschandler@gmail.com>
2020-09-21 20:26:35 -0500

    updates

--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WeatherHomeComponent } from './weather-home/weather-home.component';
 import { ForecastCardComponent } from './forecast-card/forecast-card.component';
-import { DegreesToDirectionPipe } from './degrees-to-direction.pipe';
-import { WeatherIconPipe } from './weather-icon.pipe';
+import { DegreesToDirectionPipe } from './pipes/degrees-to-direction.pipe';
+import { WeatherIconPipe } from './pipes/weather-icon.pipe';
 
 
 
